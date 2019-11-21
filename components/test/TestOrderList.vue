cc51c9212d98a746f994d38bccd626e1a665650f
longnh <hailong.nguyen95@gmail.com>
2019-11-21 09:34:28 +0700

    move files

---
+++ components/test/TestOrderList.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+    <v-subheader style="display:inline">OrderList</v-subheader>
+    <v-btn icon style="color:rgba(0,0,0,0.54)"
+           @click="clear()"><v-icon>clear</v-icon>
+    </v-btn>
+    <br>
+		<div v-for="item in items" style="padding: 10px">
+			<p>{{item.name}} (€ {{item.price}})</p>
+			<v-divider></v-divider>
+		</div>
+	</div>
+</template>
+
+<script>
+  export default {
+    name: 'OrderList',
+		data() {
+      return {
+        items: []
+			}
+		},
+		methods: {
+      pushItem(item) {
+        this.items.push(item);
+			},
+      clear() {
+        this.items = [];
+      }
+		},
+    domain: 'OrderList',
+		provide() {
+      return {
+        pushItem: this.pushItem
+			}
+		}
+  }
+</script>
+
+<style scoped>
+
+</style>
