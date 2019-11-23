1a4df2bee3c6ba0a6eccc4a6611bcbe46fcf61de
Pham Anh Tuan <tuanpa2295@gmail.com>
2019-11-23 11:13:23 +0700

    add posstore

---
+++ components/PosStore.vue
@@ -0,0 +1,29 @@
+<template>
+	<fragment/>
+</template>
+
+<script>
+  export default {
+    name: 'PosStore',
+    props: {},
+    data: function () {
+      return {
+        loginPassword: ''
+			}
+    },
+		domain: 'PosStore',
+    computed: {},
+    methods: {},
+		mounted() {
+		},
+		provide() {
+      return {
+				loginPassword: this.loginPassword
+			}
+		}
+  }
+</script>
+
+<style scoped>
+
+</style>
