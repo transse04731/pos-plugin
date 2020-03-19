9f39b8d9797c28b9eb6eda6170bacad0f42c1ad4
ThinhVu <vutrongthinhk7@gmail.com>
2020-03-19 10:05:43 +0700

    Add content render component

---
+++ components/common/ContentRender.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="height: 100vh; width: 100%">
+    <slot :name="viewName" v-bind="viewParams"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ContentRender',
+    props: {
+      value: {
+        type: Object,
+        default: () => ({ name: '', params: '' })
+      },
+    },
+    computed: {
+      viewName() {
+        if (this.value)
+          return this.value.name
+      },
+      viewParams() {
+        if (this.value)
+          return this.value.params
+      }
+    }
+  }
+</script>
