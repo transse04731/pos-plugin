e69fe95ab5bc07f2360572b2ec615df9430c9b9b
ThinhVu <vutrongthinhk7@gmail.com>
2020-03-24 09:39:31 +0700

    Load Categories Move Printer style to classes Hide +2.Printer after the user select printer 2. Remove un-used methods Correct behavior when user select item note or no print button Create Text layout item when user select type = Text

--- components/EditMenuCard/ProductEditor.vue
+++ components/EditMenuCard/ProductEditor.vue
@@ -3,91 +3,112 @@
     <!-- Product basic info -->
     <div class="product-editor__prop-grid">
       <div>Type:</div>
-      <g-select v-model="selectedProduct.type" :items="types" @input="updateProduct({ type: $event })"/>
+      <g-select :value="selectedProductLayout.type" :items="types" @input="changeType"/>
+      <template v-if="isProductLayout">
+        <div>ID: </div>
+        <g-text-field v-model="selectedProduct.id"/>
 
-      <div>ID: </div>
-      <g-text-field v-model="selectedProduct.id"/>
+        <div>Name *:</div>
+        <g-text-field v-model="selectedProduct.name" @click="dialog.showProductNameKbd = true"/>
 
-      <div>Name *:</div>
-      <g-text-field v-model="selectedProduct.name" @input="updateProduct({ name: $event })"/>
+        <div>Price:</div>
+        <g-text-field v-model="selectedProduct.price" @input="updateProduct({ price: $event })" />
 
-      <div>Price:</div>
-      <g-text-field v-model="selectedProduct.price" @input="updateProduct({ price: $event })"/>
-
-      <g-switch :value="selectedProduct.isModifier" @change="updateProduct({ isModifier: $event })"/>
-      <div>Is Modifier</div>
+        <g-switch :value="selectedProduct.isModifier" @change="updateProduct({ isModifier: $event })" />
+        <div>Is Modifier</div>
+      </template>
+      <template v-else>
+        <div>Name *:</div>
+        <g-text-field v-model="selectedProductLayout.text" @click="dialog.showTextKbd = true"/>
+      </template>
     </div>
-
-    <!-- Printer -->
-    <div>
-      <div class="product-editor__prop">
-        <span class="product-editor__label">Printer</span> <g-btn v-if="showAddPrinter2" @click="add2Printer">+2.Printer</g-btn>
-      </div>
+    <template v-if="isProductLayout">
+      <!-- Printer -->
       <div>
-        <div v-for="(item, index) in printers" :key="index">
-          <g-btn :style="getPrinterStyle(item)" @click="selectPrinter(item)" />
+        <div class="product-editor__prop">
+          <span class="product-editor__label">Printer</span>
+          <span v-if="showAddPrinter2" class="prop-option" @click="isPrinter2Select = true">+2. Printer</span>
         </div>
-        <g-btn @click="deselectPrinter">No Printer</g-btn>
-      </div>
-    </div>
-
-    <!-- Tax -->
-    <div style="display: flex">
-      <div style="width: 50%">
-        <div>Dine in Tax</div>
-        <g-grid-select mandatory v-model="selectedProduct.tax" :items="dineInTaxes">
-          <template #default="{ toggleSelect, item, index }">
-            <g-btn style="margin-right: 4px" @click="toggleSelect(item)">{{item.name}}</g-btn>
-          </template>
-          <template #selected="{ toggleSelect, item, index }">
-            <g-btn style="margin-right: 4px"  @click="toggleSelect(item)" background-color="blue" text-color="white">{{item.name}}</g-btn>
-          </template>
-        </g-grid-select>
+        <div>
+          <span v-for="(item, index) in printers"
+                :key="index"
+                :class="getPrinterClass(item._id)"
+                @click="selectPrinter(item._id)">
+            {{ item.name }}
+          </span>
+          <span v-if="!isPrinter2Select" :class="itemNoteClasses" @click="setAsItemNote">Note Item</span>
+          <span v-if="!isPrinter2Select" :class="noPrintClasses" @click="setAsNoPrint">No Print</span>
+        </div>
       </div>
 
-      <div style="width: 50%">
-        <div>Take Away Tax</div>
-        <g-grid-select mandatory multiple v-model="selectedProduct.tax2" item-text="name" item-value="value" :items="takeAwayTaxes">
-          <template #default="{ toggleSelect, item, index }">
-            <g-btn style="margin-right: 4px" @click="toggleSelect(item)">{{item.name}}</g-btn>
-          </template>
-          <template #selected="{ toggleSelect, item, index }">
-            <g-btn style="margin-right: 4px"  @click="toggleSelect(item)" background-color="blue" text-color="white">{{item.name}}</g-btn>
-          </template>
-        </g-grid-select>
+      <!-- Tax -->
+      <div style="display: flex">
+        <div style="width: 50%">
+          <div>Dine in Tax</div>
+          <g-grid-select mandatory v-model="selectedProduct.tax" :items="dineInTaxes">
+            <template #default="{ toggleSelect, item, index }">
+              <div class="prop-option" @click="toggleSelect(item)">{{item.name}}</div>
+            </template>
+            <template #selected="{ toggleSelect, item, index }">
+              <div class="prop-option prop-option--1" @click="toggleSelect(item)">{{item.name}}</div>
+            </template>
+          </g-grid-select>
+        </div>
+
+        <div style="width: 50%">
+          <div>Take Away Tax</div>
+          <g-grid-select mandatory multiple v-model="selectedProduct.tax2" :items="takeAwayTaxes">
+            <template #default="{ toggleSelect, item, index }">
+              <div class="prop-option" @click="toggleSelect(item)">{{item.name}}</div>
+            </template>
+            <template #selected="{ toggleSelect, item, index }">
+              <div class="prop-option prop-option--1" @click="toggleSelect(item)">{{item.name}}</div>
+            </template>
+          </g-grid-select>
+        </div>
       </div>
-    </div>
 
-    <!-- Color -->
-    <div>
-      <div>Color</div>
-      <color-selector :value="selectedProduct.color" :colors="colors" :item-size="25" @input="updateProductLayout({ color: $event })"/>
-    </div>
-
-    <!-- Happy hour -->
-    <div>
-      <div>Happy Hour</div>
-      <div>14:00 - 17:00 : 20%</div>
-      <div>Mo Di Mi Do Fr Sa So</div>
-    </div>
-
-    <!-- Category -->
-    <div>
-      <div>Category</div>
+      <!-- Color -->
       <div>
-        <g-grid-select madatory multiple v-model="selectedProduct.category" item-text="name" item-value="value" :items="categories">
-          <template #default="{ toggleSelect, item, index }">
-            <g-btn style="margin-right: 4px" @click="toggleSelect(item)">{{item.name}}</g-btn>
-          </template>
-          <template #selected="{ toggleSelect, item, index }">
-            <g-btn style="margin-right: 4px"  @click="toggleSelect(item)" background-color="blue" text-color="white">{{item.name}}</g-btn>
-          </template>
-        </g-grid-select>
+        <div>Color</div>
+        <color-selector :value="selectedProduct.color" :colors="colors" :item-size="25" @input="updateProductLayout({ color: $event })"/>
       </div>
-    </div>
 
+      <!-- Happy hour -->
+      <div v-if="isProductLayout">
+        <div>Happy Hour</div>
+        <div>14:00 - 17:00 : 20%</div>
+        <div>Mo Di Mi Do Fr Sa So</div>
+      </div>
+
+      <!-- Category -->
+      <div>
+        <div>Category</div>
+        <div>
+          <g-grid-select madatory multiple v-model="selectedProduct.category" item-text="name" item-value="value" :items="categories">
+            <template #default="{ toggleSelect, item, index }">
+              <g-btn style="margin-right: 4px" @click="toggleSelect(item), changeCategory(item)">{{item.name}}</g-btn>
+            </template>
+            <template #selected="{ toggleSelect, item, index }">
+              <g-btn style="margin-right: 4px"  @click="toggleSelect(item), changeCategory(item)" background-color="blue" text-color="white">{{item.name}}</g-btn>
+            </template>
+          </g-grid-select>
+        </div>
+      </div>
+    </template>
     <template>
-      <dialog-text-filter label="Product name" :default-value="selectedProduct.name" v-model="dialog.showProductNameKbd" @submit="changeProductName"/>
+      <dialog-text-filter
+          v-if="isProductLayout"
+          label="Product name"
+          :default-value="selectedProduct.name"
+          v-model="dialog.showProductNameKbd"
+          @submit="changeProductName"/>
+      <dialog-text-filter
+          v-else
+          label="Text"
+          :default-value="selectedProductLayout.text"
+          v-model="dialog.showTextKbd"
+          @submit="changeText"/>
     </template>
   </div>
 </template>
@@ -95,33 +116,35 @@
   import _ from 'lodash';
   import ColorSelector from '../common/ColorSelector';
   import GGridItemSelector from '../FnButton/components/GGridItemSelector';
+  import TableExpansionRow from '../Order/components/TableExpansionRow';
+
+  const toGSelectModel = item => ({ text: item, value: item })
 
   export default {
     name: 'ProductEditor',
-    components: { GGridItemSelector, ColorSelector },
+    components: { TableExpansionRow, GGridItemSelector, ColorSelector },
     props: {
       orderLayout: Object,
       selectedCategoryLayout: Object,
       selectedProductLayout: Object,
     },
     data: function () {
-      const toGSelectModel = (item, index) => ({ id: index, text: item, value: item })
       return {
-        colors: ['#FFFFFF', '#CE93D8', '#B2EBF2', '#C8E6C9', '#DCE775', '#FFF59D', '#FFCC80', '#FFAB91'],
-        //
+        colors: '#FFFFFF,#CE93D8,#B2EBF2,#C8E6C9,#DCE775,#FFF59D,#FFCC80,#FFAB91'.split(','),
+        // Product layout types
         types: _.map([ 'Article', 'Div.Article', 'Text', 'Menu' ], toGSelectModel),
-        //
-        showAddPrinter2: true,
+        dineInTaxes: _.map(['19%', '7%'], toGSelectModel),
+        takeAwayTaxes: _.map(['19%', '7%'], toGSelectModel),
+        // indicate whether the +2. Printer button has been clicked or not
         isPrinter2Select: false,
-        printers: [{ id:1, name:'Bar', value: 'Bar'}, { id: 2, name: 'Kitchen', value: 'Kitchen'}, { id: 3, name: 'No Printer', value: 'No Printer' }],
-        //
-        dineInTaxes: [{ id: 1, name: '19%', value: '19%' }, { id: 1, name: '7%', value: '7%' }],
-        takeAwayTaxes: [{ id: 1, name: '19%', value: '19%' }, { id: 1, name: '7%', value: '7%' }],
-        //
-        categories: [],
+        // GroupPrinter collection data
+        printers: null,
+        // Category collection data
+        categories: null,
         //
         dialog: {
           showProductNameKbd: false,
+          showTextKbd: false,
         },
       }
     },
@@ -129,61 +152,171 @@
       selectedProduct() {
         return this.selectedProductLayout.product
       },
-      selectedPrinters: {
-        get() {
-          return [
-            { _id: this.selectedProduct.groupPrinter._id, name: this.selectedProduct.groupPrinter.name },
-            { _id: this.selectedProduct.groupPrinter._id, name: this.selectedProduct.groupPrinter.name }
-          ]
-        },
-        set(values) {
-
+      showAddPrinter2() {
+        return (this.selectedProduct.groupPrinter
+            && !this.selectedProduct.groupPrinter2
+            && !this.selectedProduct.isItemNote
+            && !this.selectedProduct.isNoPrint
+            && !this.isPrinter2Select
+        )
+      },
+      isProductLayout() {
+        return this.selectedProductLayout.type !== 'Text'
+      },
+      noPrintClasses() {
+        return {
+          'prop-option': true,
+          'prop-option--1': this.selectedProduct.isNoPrint,
+        }
+      },
+      itemNoteClasses() {
+        return {
+          'prop-option': true,
+          'prop-option--1': this.selectedProduct.isItemNote,
         }
       }
     },
+    async created() {
+      await this.loadPrinters()
+      await this.loadCategories()
+    },
     methods: {
-      getPrinterStyle(printer) {
-        if (printer === this.selectedProduct.groupPrinter) {
-          return {
-            backgroundColor: 'red'
-          }
-        } else if (printer === this.selectedProduct.groupPrinter2) {
-          return {
-            backgroundColor: 'blue'
-          }
+      // categories
+      async loadCategories() {
+        this.categories = await cms.getModel('Category').find({})
+      },
+      async changeCategory(category) {
+        await this.updateProduct({ category: category._id })
+      },
+
+      // printers
+      async loadPrinters() {
+        this.printers = await cms.getModel('GroupPrinter').find({})
+      },
+      getPrinterClass(printer) {
+        return {
+          'prop-option': true,
+          'prop-option--1': this.selectedProduct.groupPrinter && this.selectedProduct.groupPrinter._id === printer,
+          'prop-option--2': this.selectedProduct.groupPrinter2 && this.selectedProduct.groupPrinter2._id === printer,
         }
       },
-      add2Printer() {
-        this.isPrinter2Select = true
-      },
-      async selectPrinter(printer) {
+      async selectPrinter(id) {
+        const printer = _.find(this.printers, p => p._id === id)
+        const change = {
+          isItemNote: false,
+          isNoPrint: false
+        }
         if (this.isPrinter2Select) {
-          this.selectedProduct.groupPrinter2 = printer
+          if (this.selectedProduct.groupPrinter !== printer) {
+            change.groupPrinter2 = printer;
+          }
+          this.isPrinter2Select = false;
         } else {
-          this.selectedProduct.groupPrinter = printer;
-          this.selectedProduct.groupPrinter2 = null
+          change.groupPrinter = printer;
+          change.groupPrinter2 = null;
         }
-        await this.updateProduct({
-          groupPrinter: this.selectedProduct.groupPrinter,
-          groupPrinter2: this.selectedProduct.groupPrinter2
-        })
+        await this.updateProduct(change)
       },
-      async deselectPrinter() {
-        this.showAddPrinter2 = false;
-        this.isPrinter2Select = false;
-        this.selectedProduct.groupPrinter = null;
-        this.selectedProduct.groupPrinter2 = null;
+      async setAsItemNote() {
         await this.updateProduct({
           groupPrinter: null,
-          groupPrinter2: null
+          groupPrinter2: null,
+          isItemNote: true,
+          isNoPrint: false,
         })
+      },
+      async setAsNoPrint() {
+        await this.updateProduct({
+          groupPrinter: null,
+          groupPrinter2: null,
+          isNoPrint: true,
+          isItemNote: false
+        });
+      },
+      // end printers
+
+      // ----
+      async changeType(type) {
+        if (this.selectedProductLayout._id) {
+          if (this.selectedProductLayout.type === type)
+            return
+
+          if (type === 'Text') {
+            if (['Article', 'Div.Article'].includes(this.selectedProductLayout.type)) {
+              // TODO: Article -> Text
+              // pseudo:
+              // 1. remove linked product
+              // 2. add text to product layout
+            }
+          } else if (['Article', 'Div.Article'].includes(type)) {
+            if (this.selectedProductLayout.type === 'Text') {
+              // TODO: Text -> Article, Div.Article
+              // pseudo:
+              // 1. clear product layout text
+              // 2. add new product
+            }
+          }
+        } else {
+          switch (type) {
+            case 'Text':
+              await this.createNewProductLayoutText(this.selectedProductLayout.text)
+              break;
+            case 'Article':
+            case 'Div.Article':
+              if (this.selectedProduct.name) {
+                // pseudo:
+                // 1. create new product
+                // 2. create new product layout linked with created product
+                await this.createNewProductLayoutWithProduct()
+              }
+              break;
+          }
+        }
+      },
+
+      async changeText(newText) {
+        if (!this.selectedProductLayout._id)
+          await this.createNewProductLayoutText(newText)
+        else
+          await this.updateProductLayout({ text: newText })
+      },
+
+      async createNewProductLayoutText(text) {
+        if (text)
+          await this.createNewProductLayout(null,{ text: text, type: 'Text' })
+      },
+
+      async createNewProductLayoutWithProduct() {
+        if (this.selectedProduct.name) {
+          const product = await cms.getModel('Product').create({ ...this.selectedProduct });
+          await this.createNewProductLayout(product._id)
+        }
       },
 
       async changeProductName(name) {
-        this.selectedProduct.name = name
-        await this.updateProduct({ name })
+        switch (this.selectedProductLayout.type) {
+          case 'Article':
+          case 'Div.Article':
+            await this.updateProduct({ name })
+            break;
+        }
       },
 
+      async createNewProductLayout(productId, extraInfo) {
+        const productLayout = {
+          product: productId,
+          ..._.pick(this.selectedProductLayout, ['top', 'left', 'color']),
+          ...extraInfo
+        }
+        const result = await cms.getModel('OrderLayout').findOneAndUpdate(
+            { 'categories._id' : this.selectedCategoryLayout._id },
+            { $push: { 'categories.$.products' : productLayout } },
+            { new: true });
+        // TODO: BUG: new orderLayout has been emitted but the result doesn't change in GUI
+        this.$emit('update:orderLayout', result)
+      },
+
+      // update color, update text
       async updateProductLayout(change) {
         if (this.selectedProductLayout._id) {
           const qry = { 'categories.products._id': this.selectedProductLayout._id }
@@ -191,26 +324,19 @@
               result[`categories.products.$.${key}`] = value
               return result
             }, {}) };
-          console.log('update', qry, 'set', set);
-          await cms.getModel('OrderLayout').findOneAndUpdate(qry, set);
+          await cms.getModel('OrderLayout').findOneAndUpdate(qry, set, { new: true });
         }
         _.assign(this.selectedProductLayout, change)
       },
 
+      // update ...
       async updateProduct(change) {
-        // skip update db in case is empty
         if (!this.selectedProduct._id) {
           if (!this.selectedProduct.name) return;
-          // add new product info: name, price, tax, tax2, category, groupPrinter, isModifier
-          const product = await cms.getModel('Product').create({ ...this.selectedProduct });
-          // add new product layout
-          const orderLayout = await cms.getModel('OrderLayout').findOneAndUpdate(
-              { 'categories._id' : this.selectedCategoryLayout._id },
-              { $push: { 'categories.$.products' : { product: product._id, ..._.pick(this.selectedProductLayout, ['top', 'left', 'color']) } } },
-              { new: true });
-          this.$emit('update:orderLayout', orderLayout)
+          await this.createNewProductLayoutWithProduct()
         } else {
           await cms.getModel('Product').findOneAndUpdate({_id: this.selectedProduct._id}, change)
+          _.assign(this.selectedProduct, change)
         }
       },
     }
@@ -227,5 +353,40 @@
       grid-auto-rows: 50px;
       align-items: center;
     }
+
+    &__label {
+      height: 30px;
+      line-height: 30px;
+      margin-bottom: 5px;
+    }
+
+    &__printer {
+      padding: 4px;
+      margin-right: 4px;
+      border-radius: 4px;
+      border: 1px solid #000;
+      cursor: pointer;
+    }
+  }
+
+  .prop-option {
+    display: inline-block;
+    padding-left: 4px;
+    padding-right: 4px;
+    cursor: pointer;
+    border: 1px solid #E0E0E0;
+    box-sizing: border-box;
+    border-radius: 2px;
+
+    &--1 {
+      background: #E3F2FD;
+      border: 1px solid #90CAF9;
+    }
+
+    &--2 {
+      background: #ffab91;
+      border: 1px solid #e6724b;
+    }
+
   }
 </style>
