f3b92c3cc7e464a30ee8abc0936bc1508ab78a10
ThinhVu <vutrongthinhk7@gmail.com>
2020-03-24 11:34:03 +0700

    Add posOrder component

---
+++ components/posOrder/posOrder.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'posOrder',
+    props: {},
+    data: function () {
+      return {}
+    },
+    computed: {},
+    methods: {}
+  }
+</script>
+<style scoped>
+</style>
