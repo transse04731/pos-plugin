a68232de2a9aa8f0737ee395791b833529e234d7
ThinhVu <vutrongthinhk7@gmail.com>
2020-03-26 18:44:57 +0700

    Unique table name

--- components/TablePlan/EditTablePlan.vue
+++ components/TablePlan/EditTablePlan.vue
@@ -47,10 +47,10 @@
             </pos-text-field>
             <div style="margin: 5px">
               <div> Color:</div>
-              <color-selector :value="roomObj.bgColor" :colors="tableColors" @input="changeRoomObjColor" :item-size="18" :badge-size="12"/>
+              <color-selector :value="roomObj.bgColor" :colors="tableColors" @input="_updateRoomObject({ bgColor: $event || 'black' })" :item-size="18" :badge-size="12"/>
               <div style="display: flex; align-items: center">
                 <span style="margin-right: 10px">Take away:</span>
-                <g-switch v-model="roomObj.takeAway" @change="changeRoomObjTakeAway"/>
+                <g-switch v-model="roomObj.takeAway" @change="_updateRoomObject({ takeAway: $event })"/>
               </div>
             </div>
           </template>
@@ -59,7 +59,7 @@
           <template v-else-if="isWall(roomObj)">
             <div style="margin: 5px">
               <div> Color:</div>
-              <color-selector :value="roomObj.bgColor" :colors="wallColors" @input="changeRoomObjColor" :item-size="18" :badge-size="12"/>
+              <color-selector :value="roomObj.bgColor" :colors="wallColors" @input="_updateRoomObject({ bgColor: $event || 'black' })" :item-size="18" :badge-size="12"/>
             </div>
           </template>
           <div style="display: flex; margin: 5px; justify-content: space-between">
@@ -110,6 +110,7 @@
     <template>
       <dialog-text-filter label="Room name" v-if="room" :default-value="room.name" v-model="dialog.showRoomNameKbd" @submit="changeRoomName"/>
       <dialog-text-filter label="Table name" v-if="roomObj" :default-value="roomObj.name" v-model="dialog.showTableNameKbd" @submit="changeTableName"/>
+      <g-snackbar v-model="showSnackBar" time-out="3000">{{tableNameExistedErrorMsg}}</g-snackbar>
     </template>
   </div>
 </template>
@@ -130,9 +131,9 @@
     props: {},
     data: function () {
       return {
-        rooms: null,
-        room: null,
-        roomObj: null,
+        rooms: [],     // entire rooms
+        room: null,    // selected room
+        roomObj: null, // selected roomObject (table, wall)
         tableColors: ['#FFFFFF', '#FBE4EC', '#EDE7F6', '#E1F5FE', '#FFFDE7', '#E8F5E9', '#EFEBE9', '#F5F5F5'],
         wallColors: ['#FFFFFF', '#CCCCCC', '#4D0019', '#404040', '#86592D', '#A6A6A6', '#FFD480', '#E4E4E4'],
         showAddNewRoomBtn: false,
@@ -140,7 +141,9 @@
           showRoomNameKbd: false,
           showTableNameKbd: false,
         },
-        defaultPath: 'item.0.item.0'
+        defaultPath: 'item.0.item.0',
+        tableNameExistedErrorMsg: '',
+        showSnackBar: false
       }
     },
     computed: {
@@ -149,16 +152,23 @@
           title: 'Restaurant', icon: 'icon-restaurant',
           items: _.map(this.rooms, this.convertRoomToSideBarItem)
         }]
+      },
+      tableNames() {
+        const tableNames = {}
+        _.each(this.rooms, room => {
+          _.each(room.roomObjects, ro => {
+            if (ro.name)
+              tableNames[ro.name] = ro._id
+          })
+        })
+        return tableNames
       }
     },
     methods: {
       async loadRooms() {
-        this.rooms = await cms.getModel('Room').find({})
-        this.showAddNewRoomBtn = true
+        this.rooms.splice(0, this.rooms.length, ...await cms.getModel('Room').find({}))
       },
       onSidebarToggle(path, toggled) {
-        // in this window, only Restaurant item is collapsible so we don't need to care about path
-        // just update the visibility of Add new button depend on toggled state
         this.showAddNewRoomBtn = toggled
       },
       selectFirstRoom() {
@@ -172,17 +182,27 @@
           icon: 'radio_button_unchecked',
           iconType: 'small',
           onClick: () => {
-            this.room = room;
+            this.$set(this, 'room', room)
             this.roomObj = null
           }
         }
       },
+      getUniqueTableName(baseName) {
+        const init = baseName || 1
+        let name = init
+        let ctr = 1
+        while(_.has(this.tableNames, `${name}`)) {
+          name = init + ctr
+          ctr++
+        }
+        return name
+      },
       async addNewRoom() {
-        const createdRoom = await cms.getModel('Room').create({ name: 'New Room' });
-        this.rooms.push(createdRoom)
+        const newName = this.getUniqueTableName()
+        const created = await cms.getModel('Room').create({ name: newName });
+        this.rooms.push(created)
       },
       async removeRoom() {
-        // todo: verify
         await cms.getModel('Room').remove({ _id: this.room._id })
         const roomIndex = _.findIndex(this.rooms, r => r === this.room)
         this.rooms.splice(roomIndex, 1)
@@ -201,61 +221,64 @@
       },
 
       selectRoomObj(roomObj) {
-        console.log('select room')
-        this.roomObj = roomObj;
+        this.$set(this, 'roomObj', roomObj);
       },
 
       // add room object
       async addNewWall() {
-        await this._addRoomObj({ size: { width: 300, height: 10 }, location: { x: 0, y: 0 }, type: 'wall', bgColor: 'black' })
+        await this._addRoomObj({
+          size: { width: 300, height: 10 },
+          location: { x: 0, y: 0 },
+          type: 'wall',
+          bgColor: 'black'
+        })
       },
       async addNewTable() {
-        await this._addRoomObj({ size: { width: 100, height: 100 }, location: { x: 0, y: 0 }, type: 'table', bgColor: 'white', name: 'New Table', takeAway: false })
+        await this._addRoomObj({
+          size: { width: 100, height: 100 },
+          location: { x: 0, y: 0 },
+          type: 'table',
+          bgColor: 'white',
+          name: this.getUniqueTableName(),
+          takeAway: false
+        })
       },
       async duplicateRoomObj() {
-        await this._addRoomObj({ ..._.cloneDeep(this.roomObj), _id: null, location: { x:0, y: 0 } })
+        await this._addRoomObj({
+          ..._.cloneDeep(this.roomObj),
+          name: this.getUniqueTableName(),
+          _id: null,
+          location: { x:0, y: 0 }
+        })
       },
       async removeRoomObj() {
-        if (this.isTable(this.roomObj)) {
-          // do smt to check if delete is allowed
-        }
         const idOfRoomObj = _.findIndex(this.room.roomObjects, ro => ro === this.roomObj);
         this.room.roomObjects.splice(idOfRoomObj, 1)
         this.roomObj = null;
-        // TODO: Remove item in Array using $pull
         await cms.getModel('Room').findOneAndUpdate({ _id: this.room._id }, { roomObjects: this.room.roomObjects })
+        await this.loadRooms()
       },
       async _addRoomObj(ro) {
-        // TODO: Do sth if update fail
         const result = await cms.getModel('Room').findOneAndUpdate(
             { _id: this.room._id },
             { $push: { roomObjects: ro } },
             { new : true });
         this.room.roomObjects.push(_.last(result.roomObjects))
       },
-
-      // update room object
-      async changeRoomObjTakeAway(takeAway) {
-        await this._updateRoomObject({ takeAway });
-      },
-      async changeRoomObjColor(bgColor) {
-        if (bgColor === null) bgColor = 'black';
-        await this._updateRoomObject({ bgColor });
-        this.roomObj.bgColor = bgColor
-      },
-      async changeRoomObjUI() {
-        await this._updateRoomObject({
-          location: this.roomObj.location,
-          size: this.roomObj.size,
-          bgColor: this.roomObj.bgColor,
-          rotate: this.roomObj.rotate || 0
-        });
+      async changeRoomObjUI(roomObj) {
+        await this._updateRoomObject(_.pick(roomObj, ['location', 'size', 'bgColor']));
       },
       async changeTableName(name) {
-        await this._updateRoomObject({name});
-        this.roomObj.name = name
+        const availName = this.getUniqueTableName(name)
+        if (availName === name) {
+          await this._updateRoomObject({name});
+        } else {
+          if (this.roomObj._id !== this.tableNames[name]) {
+            this.tableNameExistedErrorMsg = `Table "${name}" already existed, please select another name! E.g: "${availName}"`
+            this.showSnackBar = true
+          }
+        }
       },
-
       async _updateRoomObject(change) {
         const qry = { 'roomObjects._id': this.roomObj._id }
 
@@ -265,19 +288,17 @@
 
         // console.log('update', qry, 'set', set)
         await cms.getModel('Room').findOneAndUpdate(qry, set)
+
+        _.assign(this.roomObj, change)
       },
 
       back() {
-        // TODO: Use go back instead
-        // Issue: go back return exactly previous page (Dashboard -> Functions)
-        // but the problem is it doesn't refresh the UI
-        // so in case we add or remove room, the change will not be updated
-        // in Dashboard page
         this.$router.push('/view/pos-dashboard')
       }
     },
-    created() {
-      this.loadRooms()
+    async created() {
+      await this.loadRooms()
+      this.showAddNewRoomBtn = true
       this.timeInterval = setInterval(() => this.now = dayjs().format('HH:mm'), 1000)
     },
     beforeDestroy() {
