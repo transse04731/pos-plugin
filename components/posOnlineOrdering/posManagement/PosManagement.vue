f2bb483eed9a8bc1af5231926873ad60118e28aa
ThinhVu <vutrongthinhk7@gmail.com>
2020-04-03 18:34:00 +0700

    Add blank Pos Management page

---
+++ components/posOnlineOrdering/posManagement/PosManagement.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>POS Management</div>
+    <div class="pos-management__actions-bar">
+      <g-text-field-bs prepend-icon="search" placeholder="Search name or branch"/>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'PosManagement',
+    props: {},
+    data: function () {
+      return {
+        storePlaceHolder: $t('posManagement.searchPlaceholder')
+      }
+    },
+    computed: {},
+    methods: {}
+  }
+</script>
+<style scoped>
+</style>
