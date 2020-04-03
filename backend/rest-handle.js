878d0d9e2a4b387230e589804e9bf6c2100e462b
ThinhVu <vutrongthinhk7@gmail.com>
2020-04-03 18:37:41 +0700

    Change login url

--- backend/rest-handle.js
+++ backend/rest-handle.js
@@ -1,7 +1,7 @@
 const authenticateAPI = require('./api/authenticate')
 
 module.exports = cms => {
-  cms.data['loginUrl'] = '/view/login';
+  cms.data['loginUrl'] = '/view/od-login';
   cms.data['nonAuthenticateUrls'] = ['/login']
 
   cms.app.use('/user', authenticateAPI)
