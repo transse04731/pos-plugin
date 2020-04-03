f9dced0d975480211200a10f049e36e8934cd75d
ThinhVu <vutrongthinhk7@gmail.com>
2020-04-03 18:36:23 +0700

    Switch to new login page

--- backend/rest-handle.js
+++ backend/rest-handle.js
@@ -1,5 +1,8 @@
 const authenticateAPI = require('./api/authenticate')
 
 module.exports = cms => {
+  cms.data['loginUrl'] = '/view/login';
+  cms.data['nonAuthenticateUrls'] = ['/login']
+
   cms.app.use('/user', authenticateAPI)
 }
