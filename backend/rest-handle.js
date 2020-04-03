8d924b18202d34e7820ba52630c9f588e8452a68
ThinhVu <vutrongthinhk7@gmail.com>
2020-04-03 18:49:31 +0700

    Default routing

--- backend/rest-handle.js
+++ backend/rest-handle.js
@@ -5,4 +5,14 @@ module.exports = cms => {
   cms.data['nonAuthenticateUrls'] = ['/login']
 
   cms.app.use('/user', authenticateAPI)
+
+  cms.app.use(/^\/$/, async (req, res, next) => {
+    if (!req.session.userId) {
+      return res.redirect('/view/od-login')
+    }
+    if (req.session.userRole.name !== 'admin') {
+      return res.redirect('/view/od-management')
+    }
+    next()
+  })
 }
