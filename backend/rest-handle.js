c30cd9cd3726d2783769b891547b1b6ff9fcb231
ThinhVu <vutrongthinhk7@gmail.com>
2020-04-08 16:31:16 +0700

    Prevent url conflict between /view/login and /login. TODO: update permission plugin to fix this issue.

--- backend/rest-handle.js
+++ backend/rest-handle.js
@@ -1,19 +1,21 @@
 const authenticateAPI = require('./api/authenticate')
 
 module.exports = cms => {
-  cms.data['loginUrl'] = '/view/login';
+  cms.data['loginUrl'] = '/view/sign-in';
   cms.data['nonAuthenticateUrls'] = ['/login']
   cms.data['nonAuthenticateUrls'].push('/admin');
 
   cms.app.use('/user', authenticateAPI)
 
   cms.socket.on('connect', socket => {
-    socket.on('get-user-info', callback => callback(socket.request.user))
+    socket.on('get-user-info', callback => {
+      callback(socket.request.user)
+    })
   })
 
   cms.app.use(/^\/$/, async (req, res, next) => {
     if (!req.session.userId) {
-      return res.redirect('/view/login')
+      return res.redirect('/view/sign-in')
     }
     if (req.session.userRole.name !== 'admin') {
       return res.redirect('/view/management')
