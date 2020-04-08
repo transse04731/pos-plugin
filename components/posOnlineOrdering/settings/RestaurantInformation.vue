d8d40c68801ead22bd34a2b2f1303cb226bbdbbf
ThinhVu <vutrongthinhk7@gmail.com>
2020-04-08 15:58:21 +0700

    Binding data, event

--- components/posOnlineOrdering/settings/RestaurantInformation.vue
+++ components/posOnlineOrdering/settings/RestaurantInformation.vue
@@ -5,28 +5,50 @@
       <div class="restaurant-info__main--left">
         <div class="mb-3 ml-1 fw-700">Basic info</div>
         <div style="display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: 1fr 2fr 1fr 1fr 1fr 1fr; grid-gap: 5px">
-            <g-text-field-bs label="Restaurant Name" placeholder="Restaurant Name"/>
-            <g-text-field-bs label="Restaurant Phone" placeholder="Restaurant Phone"/>
+            <g-text-field-bs label="Restaurant Name"
+                             placeholder="Restaurant Name"
+                             :value="store.name"
+                             @input="$emit('update', { name: $event })"/>
+            <g-text-field-bs label="Restaurant Phone"
+                             placeholder="Restaurant Phone"
+                             :value="store.phone"
+                             @input="$emit('update', { phone: $event })"/>
             <div class="span-2">
               <p class="ml-1">Restaurant Address</p>
-              <g-textarea outlined no-resize placeholder="Address..." :rows="3"/>
+              <g-textarea outlined no-resize placeholder="Address..."
+                          :rows="3"
+                          :value="store.address"
+                          @input="$emit('update', { address: $event })"/>
             </div>
-            <g-text-field-bs label="Zip code"/>
-            <g-text-field-bs label="Town/City"/>
-            <g-text-field-bs class="span-2" label="Country"/>
-            <g-text-field-bs class="span-2" label="Currency"/>
-            <g-text-field-bs class="span-2" label="Time zone"/>
+            <g-text-field-bs label="Zip code"
+                             :value="store.zipCode"
+                             @input="$emit('update', {zipCode: $event})"/>
+            <g-text-field-bs label="Town/City"
+                             :value="store.townCity"
+                             @input="$emit('update', {townCity: $event})"/>
+            <g-text-field-bs class="span-2"
+                             label="Country"
+                             :value="store.country"
+                             @input="$emit('update', {country: $event})"/>
+            <g-text-field-bs class="span-2"
+                             label="Currency"
+                             :value="store.currency"
+                             @input="$emit('update', {currency: $event})"/>
+            <g-text-field-bs class="span-2"
+                             label="Time zone"
+                             :value="store.timeZone"
+                             @input="$emit('update', {timeZone: $event})"/>
         </div>
       </div>
       <div class="restaurant-info__main--right">
         <div class="mb-3 fw-700">Upload photo</div>
         <div class="mb-5">
           <div class="mb-2">Restaurant Photo</div>
-          <upload-zone @url="saveImage"/>
+          <upload-zone :url="store.orderHeaderImageSrc" @url="saveImage"/>
         </div>
         <div>
           <div class="mb-2">Restaurant Logo</div>
-          <upload-zone @url="saveLogo"/>
+          <upload-zone :url="store.logoImageSrc" @url="saveLogo"/>
         </div>
       </div>
     </div>
@@ -34,21 +56,27 @@
 </template>
 <script>
   import UploadZone from './UploadZone';
+  
+  // TODO:
+  // - remove old image when user change to new image
+  // - input debounce
+  
   export default {
     name: 'RestaurantInformation',
     components: { UploadZone },
-    props: {},
+    props: {
+      store: Object
+    },
     data: function () {
       return {}
     },
     computed: {},
     methods: {
       async saveImage(url) {
-        debugger
-        await this.$getService('PosOnlineOrderSettingStore').changeRestaurantInfo({ orderHeaderImageSrc: url })
+        this.$emit('update', { orderHeaderImageSrc: url })
       },
       async saveLogo(url) {
-        await this.$getService('PosOnlineOrderSettingStore').changeRestaurantInfo({ logoImageSrc: url })
+        this.$emit('update', { logoImageSrc: url })
       }
     }
   }
