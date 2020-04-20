cb804d97e9592002f2efc7a6e933b0ba03c4904e
danglam105 <danglam105@gmail.com>
2020-04-20 16:43:16 +0700

    Add ESC printing feature

--- backend/print-entire-receipt/entire-receipt-printer.js
+++ backend/print-entire-receipt/entire-receipt-printer.js
@@ -1,13 +1,13 @@
 const Vue = require('vue');
 const _ = require('lodash');
-const { renderer, print, groupArticles } = require('../print-utils/print-utils');
+const {renderer, print, groupArticles, getPrinter} = require('../print-utils/print-utils');
 
 module.exports = async function (cms) {
   cms.socket.on('connect', socket => {
-    socket.on('printEntireReceipt', async function ({ order, device }, callback) {
+    socket.on('printEntireReceipt', async function ({order, device}, callback) {
       try {
         const results = await printHandler(order, device)
-        callback({ success: true, results })
+        callback({success: true, results})
       } catch (e) {
         callbackWithError(callback, e)
       }
@@ -17,16 +17,16 @@ module.exports = async function (cms) {
   async function printHandler(order, device) {
     // get device printers
     const groupPrinters = await cms.getModel('GroupPrinter').aggregate([
-      { $unwind: { path: '$printers' } },
-      { $match: { 'printers.hardwares': device, 'type': 'entire' } },
+      {$unwind: {path: '$printers'}},
+      {$match: {'printers.hardwares': device, 'type': 'entire'}},
     ])
 
     const _groupPrinters = _.reduce(groupPrinters, (acc, printer) => {
-      const { includes, groupArticles: _groupArticles, oneReceiptForOneArticle } = printer.printers
+      const {includes, groupArticles: _groupArticles, oneReceiptForOneArticle} = printer.printers
       let items = order.items
       if (_groupArticles) items = groupArticles(items)
 
-      items = _.filter(items, function ({ groupPrinter, groupPrinter2 }) {
+      items = _.filter(items, function ({groupPrinter, groupPrinter2}) {
         if (!groupPrinter) return
         if (!includes.length) return true
         return includes.includes(groupPrinter) || printer.printers.includes.includes(groupPrinter2)
@@ -34,10 +34,10 @@ module.exports = async function (cms) {
 
       if (oneReceiptForOneArticle) {
         _.forEach(items, function (article) {
-          acc.push({ ...printer, items: [article] })
+          acc.push({...printer, items: [article]})
         })
       } else {
-        acc.push({ ...printer, items })
+        acc.push({...printer, items})
       }
 
       return acc
@@ -45,42 +45,109 @@ module.exports = async function (cms) {
 
     let results = []
     //render report
-    await Promise.all(_.map(_groupPrinters, (printer) => {
-      return new Promise((resolve, reject) => {
-        const KitchenReport = require('../../dist/Kitchen.vue')
+    await Promise.all(_.map(_groupPrinters, printer => {
+      return new Promise(async resolve => {
         const props = {
           items: printer.items,
           table: order.table,
-          printer: printer.name,
-          user: _.last(order.user).name,
+          // printer: printer.name, // not needed, these 2 depends on isKitchenReceipt (refer to Kitchen.vue)
+          // user: _.last(order.user).name,
           time: dayjs(order.date).format('HH:mm'),
           fontSize: printer.printers.fontSize,
           marginTop: printer.printers.marginTop
         }
 
-        const component = new Vue({
-          components: { KitchenReport },
-          render(h) {
-            return h('KitchenReport', { props })
-          }
-        })
+        const {escPOS} = printer.printers;
 
-        renderer.renderToString(component, {}, async (err, html) => {
-          if (err) reject(err)
-          await print(html, printer.printers)
+        if (escPOS) {
+          results.push(await printEsc(props, printer));
+        } else {
+          results.push(await printSsr(props, printer));
+        }
 
-          results.push({
-            items: printer.items,
-            printer: printer.printer,
-            name: printer.name,
-          })
-          resolve()
-        })
+        resolve();
       })
 
     }))
 
     return results
+  }
+
+  async function printEsc(props, {printers: printerInfo}) {
+    const printer = await getPrinter(printerInfo);
+    const {items, table, time} = props;
+
+    function convertMoney(value) {
+      return !isNaN(value) ? value.toFixed(2) : value
+    }
+
+    printer.alignLeft();
+    printer.setTextQuadArea();
+    printer.bold(true);
+    if (table) printer.println(`Table: ${table}`);
+
+    printer.alignRight();
+    printer.setTextNormal();
+    printer.bold(true);
+    printer.println(time);
+    printer.drawLine();
+
+    printer.setTextQuadArea();
+    printer.alignLeft();
+    items.forEach(item => {
+      printer.setTextQuadArea();
+      printer.bold(true);
+      printer.printText(`${item.quantity} x ${item.id}. ${item.name}`);
+
+      printer.setTextNormal();
+      printer.bold(true);
+      if (item.modifiers) item.modifiers.forEach(mod => {
+        printer.text(`        * ${mod.name}`);
+        if (mod.price) printer.text(` $${convertMoney(mod.price)}`);
+
+        printer.newLine();
+      });
+      if (item.separate) printer.println('************************');
+    });
+
+    printer.drawLine();
+
+    printer.setTextNormal();
+    printer.bold(true);
+    printer.alignCenter();
+    printer.println('Entire Receipt');
+
+    await printer.print();
+
+    return {
+      items: props.items,
+      printer: printerInfo,
+      name: printerInfo.name,
+    }
+  }
+
+  function printSsr(props, printer) {
+    return new Promise((resolve, reject) => {
+      const KitchenReport = require('../../dist/Kitchen.vue')
+
+      const component = new Vue({
+        components: {KitchenReport},
+        render(h) {
+          return h('KitchenReport', {props})
+        }
+      });
+
+      renderer.renderToString(component, {}, async (err, html) => {
+        if (err) reject(err);
+        await print(html, printer.printers);
+
+        resolve({
+          items: printer.items,
+          printer: printer.printer,
+          name: printer.name,
+        });
+      });
+    });
   }
 
   function callbackWithError(callback, error) {
--- backend/print-kitchen/kitchen-printer.js
+++ backend/print-kitchen/kitchen-printer.js
@@ -1,119 +1,203 @@
 const Vue = require('vue');
 const _ = require('lodash')
-const { renderer, print } = require('../print-utils/print-utils')
+const {renderer, print, getPrinter, getGroupPrinterInfo} = require('../print-utils/print-utils')
 
 module.exports = async function (cms) {
   cms.socket.on('connect', socket => {
-    socket.on('printKitchen', orderHandler)
-  })
+    socket.on('printKitchen', async ({order, device}, callback) => {
+      try {
+        const groupPrinters = await getGroupPrinterInfo(cms, device, 'kitchen');
+        const printerInfos = groupPrinters.map(e => {
+          return {name: e.name, ...e.printers}
+        })
+        const receipts = getReceiptsFromOrder(order);
 
-  async function orderHandler({ order, device }, callback) {
-    try {
-      const receipts = _.reduce(order.items, function (obj, item) {
-        function addItem(i) {
-          if (!item.groupPrinter) return;
+        for (const pritnerInfo of printerInfos) {
+          const {escPOS} = pritnerInfo
+          const receiptsForPrinter = await getReceiptsForPrinter(receipts, pritnerInfo);
+          const printData = await getPrintData(receiptsForPrinter, order, pritnerInfo);
+          let results;
 
-          const groupPrinter = i === 1 ? item.groupPrinter : item.groupPrinter2;
-          if (!groupPrinter) return;
+          if (escPOS) results = await printEscPos(printData, pritnerInfo);
+          else results = await printSsr(printData, pritnerInfo);
 
-          const receiptKey = JSON.stringify({
-            groupPrinter: groupPrinter,
-            course: item.course,
-            takeout: item.takeout
-          })
+          callback({success: true, results});
+        }
+      } catch (e) {
+        console.error(e);
+        callbackWithError(callback, e);
+      }
+    });
+  });
 
-          if (!obj[receiptKey]) obj[receiptKey] = [];
+  function getReceiptsFromOrder(order) {
+    return _.reduce(order.items, function (obj, item) {
+      function addItem(i) {
+        if (!item.groupPrinter) return;
 
-          if (!_.find(item.modifiers, m => m.groupPrinter && m.groupPrinter !== groupPrinter)) {
-            obj[receiptKey].push(item);
-          } else {
-            // case modifiers have another groupPrinter
-            const modifierGroup = _.groupBy(item.modifiers, i => i.groupPrinter ? i.groupPrinter : groupPrinter);
-            for (let _groupPrinter in modifierGroup) {
-              const _item = {
-                ...item,
-                groupPrinter: _groupPrinter,
-                modifiers: modifierGroup[_groupPrinter]
-              }
+        const groupPrinter = i === 1 ? item.groupPrinter : item.groupPrinter2;
+        if (!groupPrinter) return;
 
-              const _receiptKey = JSON.stringify({
-                groupPrinter: _groupPrinter,
-                course: item.course,
-                takeout: item.takeout
-              })
+        const receiptKey = JSON.stringify({
+          groupPrinter: groupPrinter,
+          course: item.course,
+          takeout: item.takeout
+        })
 
-              if (!obj[_receiptKey]) obj[_receiptKey] = [];
+        if (!obj[receiptKey]) obj[receiptKey] = [];
 
-              obj[_receiptKey].push(_item);
+        if (!_.find(item.modifiers, m => m.groupPrinter && m.groupPrinter !== groupPrinter)) {
+          obj[receiptKey].push(item);
+        } else {
+          // case modifiers have another groupPrinter
+          const modifierGroup = _.groupBy(item.modifiers, i => i.groupPrinter ? i.groupPrinter : groupPrinter);
+          for (let _groupPrinter in modifierGroup) {
+            const _item = {
+              ...item,
+              groupPrinter: _groupPrinter,
+              modifiers: modifierGroup[_groupPrinter]
             }
+
+            const _receiptKey = JSON.stringify({
+              groupPrinter: _groupPrinter,
+              course: item.course,
+              takeout: item.takeout
+            })
+
+            if (!obj[_receiptKey]) obj[_receiptKey] = [];
+
+            obj[_receiptKey].push(_item);
           }
         }
+      }
 
-        [1, 2].forEach(addItem);
-        return obj;
-      }, {})
-
-      const results = await printHandler(receipts, order, device)
-      callback({ success: true, results })
-    } catch (e) {
-      callbackWithError(callback, e)
-    }
+      [1, 2].forEach(addItem);
+      return obj;
+    }, {});
   }
 
-  async function printHandler(receipts, order, device) {
-    // get device printers
-    const groupPrinters = await cms.getModel('GroupPrinter').aggregate([
-      { $unwind: { path: '$printers' } },
-      { $match: { 'printers.hardwares': device, 'type': 'kitchen' } },
-    ])
+  function getReceiptsForPrinter(receipts, printer) {
+    return Object.keys(receipts)
+        .filter(key => {
+          const {groupPrinter} = JSON.parse(key);
+          return printer.name === groupPrinter;
+        })
+        .map(key => receipts[key]);
+  }
 
-    let results = []
-    //render report
-    await Promise.all(_.map(receipts, (value, key) => {
-      return new Promise((resolve, reject) => {
-        const { groupPrinter, course, takeout } = JSON.parse(key)
+  async function getPrintData(receipts, order, printer) {
+    return _.map(receipts, (value, key) => {
+      const {course, takeout} = JSON.parse(key)
 
-        const printer = groupPrinters.find(i => i.name === groupPrinter)
-        if (!printer) return
+      return {
+        items: value,
+        table: order.table,
+        printer: printer.name,
+        user: _.last(order.user).name,
+        time: dayjs(order.date).format('HH:mm'),
+        fontSize: printer.fontSize,
+        marginTop: printer.marginTop,
+        isKitchenReceipt: true,
+        course,
+        takeout
+      };
+    });
+  }
 
-        const KitchenReport = require('../../dist/Kitchen.vue')
-        const props = {
-          items: value,
-          table: order.table,
-          printer: printer.name,
-          user: _.last(order.user).name,
-          time: dayjs(order.date).format('HH:mm'),
-          fontSize: printer.printers.fontSize,
-          marginTop: printer.printers.marginTop,
-          isKitchenReceipt: true,
-          course,
-          takeout
+  async function printEscPos(printData, printerInfo) {
+    const results = [];
+
+    await Promise.all(printData.map(props => {
+      return new Promise(async resolve => {
+        const printer = await getPrinter(printerInfo);
+        const {items, table, user, time, isKitchenReceipt, course, takeout} = props;
+
+        function convertMoney(value) {
+          return !isNaN(value) ? value.toFixed(2) : value
         }
 
+        printer.alignLeft();
+        printer.setTextQuadArea();
+        printer.bold(true);
+        if (takeout) printer.println('TAKE AWAY');
+        if (table) printer.println(`Table: ${table}`);
+
+        printer.alignRight();
+        printer.setTextNormal();
+        printer.bold(true);
+        printer.println(time);
+        printer.drawLine();
+
+        printer.setTextQuadArea();
+        printer.alignLeft();
+        items.forEach(item => {
+          printer.setTextQuadArea();
+          printer.bold(true);
+          printer.printText(`${item.quantity} x ${item.id}. ${item.name}`);
+
+          printer.setTextNormal();
+          printer.bold(true);
+          if (item.modifiers) item.modifiers.forEach(mod => {
+            printer.text(`        * ${mod.name}`);
+            if (mod.price) printer.text(` $${convertMoney(mod.price)}`);
+
+            printer.newLine();
+          });
+          if (item.separate) printer.println('************************');
+        });
+
+        printer.drawLine();
+
+        printer.setTextNormal();
+        printer.bold(true);
+        printer.alignCenter();
+        if (course && course > 1) printer.println(`Course ${course}`);
+        if (isKitchenReceipt) printer.println(`${printerInfo.name} Printer - ${user}`);
+        else printer.println('Entire Receipt');
+
+        await printer.print();
+
+        results.push({
+          items: props.items,
+          printer: printerInfo,
+          name: printerInfo.name,
+        });
+        resolve();
+      });
+    }));
+
+    return results;
+  }
+
+  async function printSsr(printData, printerInfo) {
+    const results = [];
+
+    await Promise.all(printData.map(props => {
+      return new Promise((resolve, reject) => {
+        const KitchenReport = require('../../dist/Kitchen.vue')
+
         const component = new Vue({
-          components: { KitchenReport },
+          components: {KitchenReport},
           render(h) {
-            return h('KitchenReport', { props })
+            return h('KitchenReport', {props})
           }
         })
 
         renderer.renderToString(component, {}, async (err, html) => {
-          if (err) reject(err)
-          await print(html, printer.printers)
+          if (err) reject(err);
+          await print(html, printerInfo);
 
-          //todo remove from production
           results.push({
-            items: value,
-            printer: printer.printers,
-            name: printer.name,
-          })
-          resolve()
+            items: props.items,
+            printer: printerInfo,
+            name: printerInfo.name,
+          });
+          resolve();
         })
-      })
+      });
+    }));
 
-    }))
-
-    return results
+    return results;
   }
 
   function callbackWithError(callback, error) {
