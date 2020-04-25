311a466d9d938bf156c2d3afe74d96cc4af230d3
anhoev <anhoev@yahoo.de>
2020-04-26 00:35:53 +0700

    fix init demodata

--- backend/initDemoData/initDemoData.js
+++ backend/initDemoData/initDemoData.js
@@ -5,6 +5,7 @@ const path = require('path');
 module.exports = async function (cms) {
   cms.on('initData-complete', async () => {
     try {
+      console.log('initDemoData')
       const pluginPath = cms.allPlugins['pos-plugin'].pluginPath;
       const dataPath = path.join(pluginPath, 'backend', 'initDemoData', 'demoData.json');
       if (fs.existsSync(dataPath) && fs.statSync(dataPath).isFile()) {
@@ -13,11 +14,7 @@ module.exports = async function (cms) {
         for (const collection in data) {
           if (data.hasOwnProperty(collection)) {
             await Promise.all(data[collection].map(async document => {
-              if (document._id) {
-                await cms.getModel(collection).updateOne({ _id: document._id }, document, { upsert: true })
-              } else {
-                await cms.getModel(collection).create(document)
-              }
+              await cms.getModel(collection).findOneAndUpdate({_id: document._id}, document, {upsert: true, new: true})
             }))
           }
         }
