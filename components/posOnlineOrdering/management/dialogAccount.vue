85545b4ed257d1656ebce2bb52a2b61d60df7788
chuoikhungbo <quanvantruong73@gmail.com>
2020-04-25 17:12:56 +0700

    fix some layouts

--- components/posOnlineOrdering/management/dialogAccount.vue
+++ components/posOnlineOrdering/management/dialogAccount.vue
@@ -16,7 +16,7 @@
           <g-text-field-bs label="Retype Password" type="password" v-model="retypePassword"/>
         </div>
         <div class="span-2">
-          <g-select deletable-chips multiple text-field-component="GTextFieldBs" label="Group" v-model="internalStoreGroups" :items="availableGroupsViewModel"/>
+          <g-autocomplete deletable-chips multiple text-field-component="GTextFieldBs" label="Group" v-model="internalStoreGroups" :items="availableGroupsViewModel"/>
         </div>
         <div class="dialog-content__permission">
           <p class="span-2 ml-2 mt-3">Permission</p>
@@ -153,7 +153,7 @@
        margin-right: 0 !important;
      }
 
-     .g-combobox ::v-deep .input {
+     .g-autocomplete ::v-deep .input {
        display: flex;
        flex-wrap: wrap;
        height: 96px;
@@ -171,7 +171,7 @@
        }
      }
 
-     .g-combobox ::v-deep .bs-tf-append-inner {
+     .g-autocomplete ::v-deep .bs-tf-append-inner {
        display: none;
      }
    }
