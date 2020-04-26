5bd31d68d40c3b5685cc8f1dae91303e119f21d0
transse04731 <triracle@gmail.com>
2020-04-26 14:38:04 +0700

    update script to download apktool in gradle

---
+++ pkg/android-dist/app/download-apktool.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/osx/apktool -o /usr/local/bin/apktool
+wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.4.1.jar -O /usr/local/bin/apktool.jar
+chmod +x /usr/local/bin/apktool
+chmod +x /usr/local/bin/apktool.jar
