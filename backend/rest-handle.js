39c2611500015563144bfb533ef95138c14f2e79
ThinhVu <vutrongthinhk7@gmail.com>
2020-04-26 16:14:26 +0700

    Hanlde /app routes

--- backend/rest-handle.js
+++ backend/rest-handle.js
@@ -1,5 +1,6 @@
 const deviceAPI = require('./api/device')
 const storeAPI = require('./api/store')
+const appManagementAPI = require('./api/appManagement')
 
 module.exports = cms => {
   cms.data['loginUrl'] = '/sign-in';
@@ -7,6 +8,7 @@ module.exports = cms => {
 
   cms.app.use('/device', deviceAPI)
   cms.app.use('/store', storeAPI)
+  cms.app.use('/app', appManagementAPI)
 
   // NOTE: If health-check API URL is changed, the URL used on frontend must be changed accordingly
   cms.app.get('/health-check', (req, res) => res.status(200).send('OK'));
