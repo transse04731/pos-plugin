fe29527b505507dc4a1d2dbdccd7f9c04552a99a
transse04731 <triracle@gmail.com>
2020-04-26 14:44:57 +0700

    change wget to curl

--- pkg/android-dist/app/download-apktool.sh
+++ pkg/android-dist/app/download-apktool.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/osx/apktool -o /usr/local/bin/apktool
-wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.4.1.jar -O /usr/local/bin/apktool.jar
+curl -L https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.4.1.jar -o /usr/local/bin/apktool.jar
 chmod +x /usr/local/bin/apktool
 chmod +x /usr/local/bin/apktool.jar
