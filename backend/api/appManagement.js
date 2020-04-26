bced0e6c4f43f1f2ce5fa98403dfdae7d9a2b5c8
ThinhVu <vutrongthinhk7@gmail.com>
2020-04-26 18:26:23 +0700

    Create new app if app is not existed yet

--- backend/api/appManagement.js
+++ backend/api/appManagement.js
@@ -4,11 +4,9 @@ const router = express.Router()
 router.post('/meta-data', async (req, res) => {
   try {
     const {uploadPath, version, type, note, group, base, release} = req.body
-    const app = await cms.getModel('App').findOne({name: group})
-    if (!app) {
-      res.json({ok:false, message: `App ${group} not found`})
-      return
-    }
+    let app = await cms.getModel('App').findOne({name: group})
+    if (!app)
+      app = await cms.getModel('App').create({ name: group })
     await cms.getModel('AppItem').create({ version, type, changeLog: note, uploadPath, uploadDate: new Date(), app: app._id, baseVersion: base, release })
     res.json({ok: true})
   } catch (e) {
