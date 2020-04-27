9d5ddf17b63c50f2abe4bde4b218ca8e251f01fb
transse04731 <triracle@gmail.com>
2020-04-27 16:03:36 +0700

    upload patch

---
+++ pkg/uploadNewVersionAndPatch.js
@@ -0,0 +1,24 @@
+const uploader = require('./upload');
+const argv = require('yargs').argv;
+const upload = new uploader({domain: argv.domain, apiBaseUrl: '/cms-files'});
+const version = require('../package').version;
+const path = require('path');
+console.log("The domain is", argv.domain);
+upload({
+  filePath: path.resolve(__dirname, './android-dist/originalBuild/app-release.apk'),
+  group: 'POS_Android',
+  version: version,
+  type: 'APK',
+  base: null,
+  release: 'Stable',
+  note: 'New version'
+})
+upload({
+  filePath: path.resolve(__dirname, './android-dist/originalBuild/app/assets/appAssets/patch/patch_signed_7zip.apk'),
+  group: 'POS_Android',
+  version: version,
+  type: 'PATCH',
+  base: null,
+  release: 'Stable',
+  note: 'New version'
+})
